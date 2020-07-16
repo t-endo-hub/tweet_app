057cd63d97ae87d4b3b6a45e5e96f2a4994bed9f
vagrant <vagrant@localhost.localdomain>
2020-07-16 07:11:12 +0000

    [Finish]likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :likes
 	has_many :liked_users, through: :likes, source: :user
+	
 	validates :title, presence: true
 	validates :body, presence: true
 	attachment :image
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 	has_many :posts, dependent: :destroy
 	has_many :likes, dependent: :destroy
 	has_many :liked_posts, through: :likes, source: :post
+
+
 	validates :name, presence: true
 	validates :email, presence: true
   # Include default devise modules. Others available are:
